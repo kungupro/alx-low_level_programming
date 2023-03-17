e922001468c34c704cd9cff20455ec66b432cce5
kungupro <gkgatumu@gmail.com>
2023-03-17 10:53:45 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The main input
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
