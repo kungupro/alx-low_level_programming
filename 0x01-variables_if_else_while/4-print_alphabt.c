db49e3e88516a3ae992aaee0a30d8db83b8b004b
kungupro <gkgatumu@gmail.com>
2023-03-17 11:10:05 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main input
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+
+}
