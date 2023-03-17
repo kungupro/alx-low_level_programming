05639f94ef268b434ea84febfaab80c6021c4554
kungupro <gkgatumu@gmail.com>
2023-03-17 11:41:46 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: Always zero upon success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
