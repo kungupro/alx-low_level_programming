a6c7bbb0b7b927142a67ab141f626f6da9a870a5
kungupro <gkgatumu@gmail.com>
2023-03-22 01:17:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -The main up
+ *
+ * _putchar - prints the _putchar alphabets
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
