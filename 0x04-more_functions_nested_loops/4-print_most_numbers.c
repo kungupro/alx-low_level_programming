46eb31b131ba2a6944737bd328ac4a567f1e0f56
kungupro <gkgatumu@gmail.com>
2023-03-24 05:36:27 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 'o' ; c <= '9' ; c++)
+	{
+		if (c != '2' && c != '4')
+			_puthar(c);
+	}
+	_putchar('\n');
+}
