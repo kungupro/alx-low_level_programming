3b34bb722c22937332fc017ab711783330081f82
kungupro <gkgatumu@gmail.com>
2023-04-27 12:09:44 -0700

    print message before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - print the messages
+ *
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
