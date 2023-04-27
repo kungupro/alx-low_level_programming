832425c380cfd424591e68e1a26917bda71cd36b
kungupro <gkgatumu@gmail.com>
2023-04-27 08:30:15 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,20 +1,36 @@
 #include <stdlib.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: - pointer to the string
+ * add_node  - adds a new node
+ * @head: - double  pointer to the list
+ * @str: New string to be added
+ * Return: Returns the new node
  *
- * Return: The number of elements
  */
-size_t list_len(const list_t *h)
-{
-	size_t count = 0;
 
-	while (h)
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
 	{
-		count++;
-		h = h->next;
+		return (NULL);
 	}
-	return (count);
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
