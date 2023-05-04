1ecdb34dbd83d8585b284c43a66eed049da22765
kungupro <gkgatumu@gmail.com>
2023-05-04 09:14:44 -0700

    function that sets the value of a bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,7 +5,7 @@
  * @n: totoal number of bits
  * @index: the position of the bit to  be set
  *
- * Return: i on success, -1 otherwise
+ * Return: i on success, or -1 otherwise
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
